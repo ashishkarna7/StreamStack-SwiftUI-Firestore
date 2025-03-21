baa48d53f6954ac6ac9ece16da9d0e8fbfd13ddd
Ashish Karna <ashish.karna7@gmail.com>
2025-03-21 23:23:26 +0545

    Fix: Added xcode project

---
+++ StreamStack/StreamStack/StreamStackApp.swift
@@ -0,0 +1,32 @@
+//
+//  StreamStackApp.swift
+//  StreamStack
+//
+//  Created by Ashish Karna on 21/03/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StreamStackApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ StreamStack/StreamStackTests/StreamStackTests.swift
@@ -0,0 +1,17 @@
+//
+//  StreamStackTests.swift
+//  StreamStackTests
+//
+//  Created by Ashish Karna on 21/03/2025.
+//
+
+import Testing
+@testable import StreamStack
+
+struct StreamStackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
